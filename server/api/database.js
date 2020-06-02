00e88f9e362b290d8d3d143911c6dbefb12d46b1
ben fozard <bf17813@my.bristol.ac.uk>
2020-06-02 22:41:18 +0100

    updateScore and removeUser

--- server/api/database.js
+++ server/api/database.js
@@ -35,23 +35,44 @@ db.serialize(create);
 
 function create() {
   /////////////////////////////////////////////////////////////////////////////////////////////// FOR DEVELOPMENT ONLY /////////////////////////////////////////////////////////////////////////////////////
-  db.run("DROP TABLE IF EXISTS user");
+  // db.run("DROP TABLE IF EXISTS user");
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   db.run(`CREATE TABLE user (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       username text UNIQUE,
-      email text UNIQUE)`);
-  // var insert = 'INSERT INTO user (username, email) VALUES (?)';
-  // db.run(insert, ['isaac', 'il17557@bristol.ac.uk']);
-  db.run("INSERT INTO user (username, email) VALUES ('isaac', 'il17557@bristol.ac.uk')");
+      email text UNIQUE,
+      highscore INTEGER(8))`,
+      (err) => {
+        if (err) {
+        //Table already created
+      } else {
+        //Table just created, populate
+        db.serialize(addUser('Isaac', 'il17557@bristol.ac.uk'));
+        db.serialize(addUser('Ben', 'bf17813@bristol.ac.uk'));
+      }
+    });
 }
 
+// db.serialize(removeUser('Ben'));
+// db.serialize(updateHighscore('Ben', 100));
 
-db.serialize(addUser('Ben', 'bf17813@bristol.ac.uk'));
+// db.serialize(addUser('Isaac', 'il17557@bristol.ac.uk'));
+// db.serialize(addUser('Ben', 'bf17813@bristol.ac.uk'));
 
 function addUser(username, email) {
-  var insert = 'INSERT INTO user (username, email) VALUES (?, ?)';
-  db.run(insert, [username, email]);
+  //New user has highscore of 0
+  var insert = 'INSERT INTO user (username, email, highscore) VALUES (?, ?, ?)';
+  db.run(insert, [username, email, 0]);
+}
+
+function removeUser(username) {
+  var remove = 'DELETE FROM user WHERE username = (?)';
+  db.run(remove, [username]);
+}
+
+function updateHighscore(username, highscore) {
+  var update = 'UPDATE user SET highscore = (?) WHERE username = (?)';
+  db.run(update, [highscore, username]);
 }
 
 
