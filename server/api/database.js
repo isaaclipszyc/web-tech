1bd1a1cba4e7772baf49dffa13defefec8d2dd76
ben fozard <bf17813@my.bristol.ac.uk>
2020-06-02 19:36:44 +0100

    email and addUser

--- server/api/database.js
+++ server/api/database.js
@@ -1,32 +1,59 @@
-var sqlite3 = require('sqlite3').verbose();
+// var sqlite3 = require('sqlite3').verbose();
+// const DBSOURCE = "db.sqlite";
+//
+// //initialises database
+// let db = new sqlite3.Database(DBSOURCE, (err) => {
+//     if (err) {
+//       // Cannot open database
+//       console.error(err.message);
+//       throw err
+//     }else{
+//         console.log('Connected to the SQLite database.');
+//         db.run(`CREATE TABLE user (
+//             id INTEGER PRIMARY KEY AUTOINCREMENT,
+//             username text UNIQUE,
+//             CONSTRAINT username_unique UNIQUE (username),
+//             )`,
+//         (err) => {
+//             if (err) {
+//                 // Table already created
+//             }else{
+//                 // Table just created, creating some rows
+//                 var insert = 'INSERT INTO user (username) VALUES (?)';
+//                 db.run(insert, ["isaac"]);
+//                 db.run(insert, ["ben"]);
+//             }
+//         });
+//     }
+// });
 
-const DBSOURCE = "db.sqlite";
+"use strict";
+var sqlite3 = require("sqlite3");
+var db = new sqlite3.Database("data.db");
 
-//initialises database
-let db = new sqlite3.Database(DBSOURCE, (err) => {
-    if (err) {
-      // Cannot open database
-      console.error(err.message);
-      throw err
-    }else{
-        console.log('Connected to the SQLite database.');
-        db.run(`CREATE TABLE user (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            username text UNIQUE, 
-            CONSTRAINT username_unique UNIQUE (username)
-            )`,
-        (err) => {
-            if (err) {
-                // Table already created
-            }else{
-                // Table just created, creating some rows
-                var insert = 'INSERT INTO user (username) VALUES (?)';
-                db.run(insert, ["isaac"]);
-                db.run(insert, ["ben"]);
-            }
-        });  
-    }
-});
+db.serialize(create);
+
+function create() {
+  /////////////////////////////////////////////////////////////////////////////////////////////// FOR DEVELOPMENT ONLY /////////////////////////////////////////////////////////////////////////////////////
+  db.run("DROP TABLE IF EXISTS user");
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  db.run(`CREATE TABLE user (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      username text UNIQUE,
+      email text UNIQUE)`);
+  // var insert = 'INSERT INTO user (username, email) VALUES (?)';
+  // db.run(insert, ['isaac', 'il17557@bristol.ac.uk']);
+  db.run("INSERT INTO user (username, email) VALUES ('isaac', 'il17557@bristol.ac.uk')");
+}
+
+
+db.serialize(addUser('Ben', 'bf17813@bristol.ac.uk'));
+
+function addUser(username, email) {
+  var insert = 'INSERT INTO user (username, email) VALUES (?, ?)';
+  db.run(insert, [username, email]);
+}
+
 
 const getUsers = (request, response) => {
     var sql = "select * from user"
