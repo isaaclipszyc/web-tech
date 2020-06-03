41f245a8ac27cf7fb35930d66dbe8c8124e5ca19
ben fozard <bf17813@my.bristol.ac.uk>
2020-06-03 12:10:37 +0100

    Leaderboard and prepared statements

--- server/api/database.js
+++ server/api/database.js
@@ -35,49 +35,90 @@ db.serialize(create);
 
 function create() {
   /////////////////////////////////////////////////////////////////////////////////////////////// FOR DEVELOPMENT ONLY /////////////////////////////////////////////////////////////////////////////////////
-  // db.run("DROP TABLE IF EXISTS user");
+  db.run("DROP TABLE IF EXISTS user");
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   db.run(`CREATE TABLE user (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       username text UNIQUE,
       email text UNIQUE,
+      password text,
       highscore INTEGER(8))`,
       (err) => {
         if (err) {
         //Table already created
       } else {
         //Table just created, populate
-        db.serialize(addUser('Isaac', 'il17557@bristol.ac.uk'));
-        db.serialize(addUser('Ben', 'bf17813@bristol.ac.uk'));
+        addUser('Isaac', 'il17557@bristol.ac.uk', 'abc');
+        addUser('Ben', 'bf17813@bristol.ac.uk', 'def');
+        // removeUser('Ben');
+        // updateHighscore('Isaac', 100);
       }
     });
 }
 
-// db.serialize(removeUser('Ben'));
-// db.serialize(updateHighscore('Ben', 100));
+// addUser('Ben', 'bf17813@bristol.ac.uk', 'def');
 
-// db.serialize(addUser('Isaac', 'il17557@bristol.ac.uk'));
-// db.serialize(addUser('Ben', 'bf17813@bristol.ac.uk'));
-
-function addUser(username, email) {
+//Password should be hashed client-side before being sent to this function for storage
+async function addUser(username, email, hashedPsword) {
   //New user has highscore of 0
-  var insert = 'INSERT INTO user (username, email, highscore) VALUES (?, ?, ?)';
-  db.run(insert, [username, email, 0]);
+  var ps = db.prepare('INSERT INTO user (username, email, password, highscore) VALUES (?, ?, ?, ?)');
+  await ps.run(username, email, hashedPsword, 0, (err) => {
+    if (err) {
+      //Fields don't match
+    } else {
+      //Insertion completed
+    }
+  });
+  await ps.finalize();
 }
 
-function removeUser(username) {
-  var remove = 'DELETE FROM user WHERE username = (?)';
-  db.run(remove, [username]);
+async function removeUser(username) {
+  var ps = db.prepare('DELETE FROM user WHERE username = (?)');
+  await ps.run(username, (err) => {
+    if (err) {
+      //Fields don't match
+    } else {
+      //Insertion completed
+    }
+  });
+  await ps.finalize();
 }
 
-function updateHighscore(username, highscore) {
-  var update = 'UPDATE user SET highscore = (?) WHERE username = (?)';
-  db.run(update, [highscore, username]);
+function checkLogin(username, hashedPsword) {
+  // var find = 'SELECT EXISTS(SELECT 1 FROM user WHERE username = (?) AND password = (?))';
+  // db.run(find, [username, hashedPsword]);
+}
+
+async function updateHighscore(username, highscore) {
+  var ps = db.prepare('UPDATE user SET highscore = (?) WHERE username = (?)');
+  await ps.run(highscore, username, (err) => {
+    if (err) {
+      //Fields don't match
+    } else {
+      //Insertion completed
+    }
+  });
+  await ps.finalize();
 }
 
 
 const getUsers = (request, response) => {
-    var sql = "select * from user"
+    var sql = "SELECT * FROM user"
+    var params = []
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+            response.status(400).json({"error":err.message});
+          return;
+        }
+        response.json({
+            "message":"success",
+            "data":rows
+        })
+      });
+}
+
+const getLeaderboard = (request, response) => {
+    var sql = "SELECT username, highscore FROM user ORDER BY highscore DESC"
     var params = []
     db.all(sql, params, (err, rows) => {
         if (err) {
